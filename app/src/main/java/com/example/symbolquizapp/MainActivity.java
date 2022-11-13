d83060852779849b3243807a988619ae6a065ccc
ahmad880 <aahhmmaadd.bkr@gmail.com>
2022-11-13 22:29:33 +0500

    Creating reference for views

--- app/src/main/java/com/example/symbolquizapp/MainActivity.java
+++ app/src/main/java/com/example/symbolquizapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.symbolquizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA,ansB,ansC,ansD;
+    Button submitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        totalQuestionsTextView=findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        ansA=findViewById(R.id.ans_a);
+        ansB=findViewById(R.id.ans_b);
+        ansC=findViewById(R.id.ans_c);
+        ansD=findViewById(R.id.ans_d);
+        submitBtn=findViewById(R.id.submit_btn);
     }
 }
