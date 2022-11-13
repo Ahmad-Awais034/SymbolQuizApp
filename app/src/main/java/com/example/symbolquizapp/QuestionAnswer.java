bf5e279efb2461475f33a72a0c5a51d055d97a87
ahmad880 <aahhmmaadd.bkr@gmail.com>
2022-11-13 22:26:53 +0500

    creating model class(Question answer)

---
+++ app/src/main/java/com/example/symbolquizapp/QuestionAnswer.java
@@ -0,0 +1,34 @@
+package com.example.symbolquizapp;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "+ is symbol of",
+            "> is a symbol of",
+            "= is a symbol of",
+            "% is a symbol of",
+            "< is a symbol of",
+            "- is a symbol of",
+            "× is a symbol of"
+
+    };
+    public static String choices[][]={
+            {"subtraction","addition","division","percentage"},
+            {"subtraction","greater than","division","percentage"},
+            {"equals to","addition","division","percentage"},
+            {"subtraction","addition","division","percentage"},
+            {"subtraction","addition","less than","percentage"},
+            {"subtraction","addition","division","percentage"},
+            {"multiplication","addition","division","percentage"}
+
+
+    };
+    public static String correctAnswers[]={
+            "addition",
+            "greater than",
+            "equals to",
+            "percentage",
+            "less than",
+            "subtraction",
+            "multiplication"
+    };
+}
